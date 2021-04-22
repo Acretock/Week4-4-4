6613938f4931aea4251701e37cec081cd2efad01
Acretock <georgoncharov@gmail.com>
2021-04-22 19:23:11 +0500

    v1.0

--- Week4-4-4/Week4-4-4.cpp
+++ Week4-4-4/Week4-4-4.cpp
@@ -1,25 +1,63 @@
 #include <iostream>
 #include <exception>
+#include <ctime>
 #include <string>
 using namespace std;
 
+string AskTimeServerTest() {
+    // ƒл€ тестировани€ повставл€йте сюда код, реализующий различное поведение этой функии:
+       // нормальный возврат строкового значени€
+       // выброс исключени€ system_error
+       // выброс другого исключени€ с сообщением.
+    int a;
+    cin >> a;
+    if (a == 1)
+        return "09:11:42";
+    if (a == 2) {
+        throw system_error(error_code()); 
+    }
+    if (a == 3) {
+        exception e("this is wrong");
+        throw (e);
+    }
+     
+}
+
 string AskTimeServer() {
-    /* ƒл€ тестировани€ повставл€йте сюда код, реализующий различное поведение этой функии:
-       * нормальный возврат строкового значени€
-       * выброс исключени€ system_error
-       * выброс другого исключени€ с сообщением.
-    */
+    // ƒл€ тестировани€ повставл€йте сюда код, реализующий различное поведение этой функии:
+       // нормальный возврат строкового значени€
+       // выброс исключени€ system_error
+       // выброс другого исключени€ с сообщением.
+    int a;
+    cin >> a;
+    if (a == 1)
+        return "09:11:42";
+    if (a == 2) {
+        throw system_error(error_code());
+    }
+    if (a == 3) {
+        exception e("this is wrong");
+        throw (e);
+    }
+
 }
 
 class TimeServer {
 public:
     string GetCurrentTime() {
-        /* –еализуйте этот метод:
-            * если AskTimeServer() вернула значение, запишите его в last_fetched_time и верните
-            * если AskTimeServer() бросила исключение system_error, верните текущее значение
-            пол€ last_fetched_time
-            * если AskTimeServer() бросила другое исключение, пробросьте его дальше.
-        */
+            // если AskTimeServer() вернула значение, запишите его в last_fetched_time и верните
+            // если AskTimeServer() бросила исключение system_error, верните текущее значение пол€ last_fetched_time
+            // если AskTimeServer() бросила другое исключение, пробросьте его дальше.
+        try {
+            last_fetched_time = AskTimeServer();    // if no exeption was thrown
+            return last_fetched_time;
+        }
+        catch (system_error&e) {                    
+            return last_fetched_time;               //sys error
+        }
+        catch (exception& e) {                      // any other exeption 
+            throw (e);
+        }
     }
 
 private:
